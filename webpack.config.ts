aa4319bef230925976970037aef1781e920d6318
bartoval <vabarjs@gmail.com>
2024-03-12 22:33:40 +0100

    update deps

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,22 +1,20 @@
 import * as path from 'path';
 
 import { ConsoleRemotePlugin } from '@openshift-console/dynamic-plugin-sdk-webpack';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
 import TsConfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
-import type { Configuration as WebpackConfiguration } from 'webpack';
+import * as webpack from 'webpack';
 import type { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const ROOT = process.cwd();
 
-interface Configuration extends WebpackConfiguration {
-  devServer?: WebpackDevServerConfiguration;
-}
-
-const config: Configuration = {
+const config: webpack.Configuration & {
+  devServer: WebpackDevServerConfiguration;
+} = {
   mode: 'development',
-  // No regular entry points. The remote container entry is handled by ConsoleRemotePlugin.
-  entry: {},
   context: path.resolve(__dirname, 'src'),
+  entry: {},
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name]-bundle.js',
@@ -30,30 +28,42 @@ const config: Configuration = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: true
-        }
+        test: /\.(jsx?|tsx?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json')
+            }
+          }
+        ]
       },
       {
-        test: /\.css$/,
+        test: /\.s?(css)$/,
         use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|woff2?|ttf|eot|otf)(\?.*$|$)/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/[name].[ext]'
-        }
-      },
-      {
-        test: /\.m?js/,
-        resolve: {
-          fullySpecified: false
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[name].[ext]'
         }
       }
     ]
+  },
+
+  plugins: [
+    new ConsoleRemotePlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: path.resolve(__dirname, 'locales'), to: 'locales' }]
+    })
+  ],
+
+  devtool: 'source-map',
+  optimization: {
+    chunkIds: 'named',
+    minimize: false
   },
 
   devServer: {
@@ -69,19 +79,6 @@ const config: Configuration = {
     devMiddleware: {
       writeToDisk: true
     }
-  },
-
-  plugins: [
-    new ConsoleRemotePlugin(),
-    new CopyWebpackPlugin({
-      patterns: [{ from: path.resolve(__dirname, 'locales'), to: 'locales' }]
-    })
-  ],
-
-  devtool: 'source-map',
-  optimization: {
-    chunkIds: 'named',
-    minimize: false
   }
 };
 
@@ -95,7 +92,6 @@ if (process.env.NODE_ENV === 'production') {
     config.optimization.chunkIds = 'deterministic';
     config.optimization.minimize = true;
   }
-  config.devtool = false;
 }
 
 export default config;
